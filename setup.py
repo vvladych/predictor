1e46588322e6d7a69ec824aade3c3203b7c97b43
vladych <vlad@vladych.nonexist.com>
2015-12-30 01:25:01 +0100

    first commit

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+__author__ = "vvladych"
+__date__ = "$30.12.2015 23:12:01$"
+
+setup (
+  name = 'predictor',
+  version = '0.1',
+  packages = find_packages(),
+  install_requires=['psycopg2'],
+  author = 'vvladych',
+  author_email = '',
+  summary = 'Predictions management',
+  url = '',
+  license = '',
+  long_description= 'Predictions',
+  # could also include long_description, download_url, classifiers, etc.
+)
