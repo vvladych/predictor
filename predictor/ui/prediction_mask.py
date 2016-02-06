2554c160f510943b16e2e491b11c3bfc71943198
vladych <vlad@vladych.nonexist.com>
2016-02-06 23:10:42 +0100

    added common name to prediction overview, dialogs in extended treeview have parent window

--- predictor/ui/prediction_mask.py
+++ predictor/ui/prediction_mask.py
@@ -19,32 +19,33 @@ class PredictionExtTreeview(ExtendedTreeView):
 
     dao_type = PredictionDAO
 
-    def __init__(self, columns, start_row=0, rows_per_page=0, on_row_select_callback=None):
-        super(PredictionExtTreeview, self).__init__(columns, start_row, rows_per_page, on_row_select_callback)
+    def __init__(self, main_window, columns, start_row=0, rows_per_page=0, on_row_select_callback=None):
+        super(PredictionExtTreeview, self).__init__(main_window, columns, start_row, rows_per_page, on_row_select_callback)
 
     def append_treedata_row(self, row):
         self.treeview.treemodel.append(["%s" % row.uuid, "publisher", "%s" % row.created_date, "%s" % row.commonname])
 
     def on_menu_item_new(self, widget):
-        new_prediction_dialog = PredictionNewDialog(None)
+        new_prediction_dialog = PredictionNewDialog(self.main_window)
         response = new_prediction_dialog.run()
 
         if response == Gtk.ResponseType.OK:
             new_prediction_dialog.perform_insert()
 
         elif response == Gtk.ResponseType.CANCEL:
-            show_info_dialog(None, "Canceled")
+            show_info_dialog(self.main_window, "Canceled")
         else:
-            show_info_dialog(None, "Unknown action")
+            show_info_dialog(self.main_window, "Unknown action")
 
         new_prediction_dialog.destroy()
         self.reset_treemodel()
 
 
 class PredictionMask(AbstractMask):
-    
+
+    dao_type = PredictionDAO
+
     def __init__(self, main_window):
-        self.dao_type = PredictionDAO
         super(PredictionMask, self).__init__(main_window)
 
     def create_overview_treeview(self):
@@ -52,11 +53,11 @@ class PredictionMask(AbstractMask):
                        TreeviewColumn("Publisher", 1, False),
                        TreeviewColumn("Date", 2, False),
                        TreeviewColumn("Prediction", 3, False)]
-        self.overview_treeview = PredictionExtTreeview(treecolumns, 0, 20, self.on_row_select)
+        self.overview_treeview = PredictionExtTreeview(self.main_window, treecolumns, 0, 20, self.on_row_select)
 
     def on_row_select(self, prediction_uuid):
         self.clear_main_middle_pane()
-        prediction = self.dao_type(prediction_uuid)
+        prediction = self.__class__.dao_type(prediction_uuid)
         prediction.load()
         self.main_middle_pane.pack_start(PredictionOverviewWindow(self, prediction), False, False, 0)
         self.main_middle_pane.show_all()
