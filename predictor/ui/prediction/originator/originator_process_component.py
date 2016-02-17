58054a40632f598c7013e2ded7f0e3c33ba72a3d
vlad <vvladych@vvladych.com>
2016-02-17 21:18:03 +0100

    added new folders

---
+++ predictor/ui/prediction/originator/originator_process_component.py
@@ -0,0 +1,214 @@
+"""
+Created on 19.05.2015
+
+@author: vvladych
+"""
+
+from gi.repository import Gtk
+
+from predictor.ui.prediction.abstract_data_process_component import AbstractDataOverviewComponent, AbstractDataManipulationComponent, AbstractDataProcessComponent
+
+from predictor.ui.ui_tools import TreeviewColumn, show_info_dialog
+
+#from forecastmgmt.model.person import Person
+#from forecastmgmt.model.organisation import Organisation
+#from forecastmgmt.model.originator import Originator
+#from forecastmgmt.model.forecast_originator import ForecastOriginator
+#from forecastmgmt.model.originator_person import OriginatorPerson
+#from forecastmgmt.model.originator_organisation import OriginatorOrganisation
+#from forecastmgmt.dao.db_connection import get_db_connection
+import psycopg2.extras
+
+
+class OriginatorProcessComponent(AbstractDataProcessComponent):
+    
+    def __init__(self, prediction):
+        super(OriginatorProcessComponent, self).__init__(OriginatorManipulationComponent(prediction, OriginatorOverviewComponent(prediction)))
+
+
+class OriginatorManipulationComponent(AbstractDataManipulationComponent):
+    
+    def __init__(self, forecast, overview_component):
+        super(OriginatorManipulationComponent, self).__init__(overview_component)
+        self.forecast=forecast
+        
+    def create_layout(self, parent_layout_grid, row):
+        
+        originator_label = Gtk.Label("Originator")
+        originator_label.set_justify(Gtk.Justification.LEFT)
+        parent_layout_grid.attach(originator_label,0,row,1,1)
+        
+        row+=1
+
+        person_originator_label = Gtk.Label("Person")
+        person_originator_label.set_justify(Gtk.Justification.LEFT)
+        parent_layout_grid.attach(person_originator_label,0,row,1,1)
+
+        
+        self.person_combobox_model=self.populate_person_combobox_model()
+        self.person_combobox=Gtk.ComboBox.new_with_model_and_entry(self.person_combobox_model)
+        self.person_combobox.set_entry_text_column(1)
+        parent_layout_grid.attach(self.person_combobox,1,row,1,1)
+        
+        self.add_person_button=Gtk.Button("Add", Gtk.STOCK_ADD)
+        parent_layout_grid.attach(self.add_person_button,2,row,1,1)
+        self.add_person_button.connect("clicked", self.add_person_action)
+
+        row += 1
+
+        organisation_originator_label = Gtk.Label("Organisation")
+        organisation_originator_label.set_justify(Gtk.Justification.LEFT)
+        parent_layout_grid.attach(organisation_originator_label,0,row,1,1)
+
+        
+        self.organisation_combobox_model=self.populate_organisation_combobox_model()
+        self.organisation_combobox=Gtk.ComboBox.new_with_model_and_entry(self.organisation_combobox_model)
+        self.organisation_combobox.set_entry_text_column(1)
+        parent_layout_grid.attach(self.organisation_combobox,1,row,1,1)
+        
+        self.add_organisation_button=Gtk.Button("Add", Gtk.STOCK_ADD)
+        parent_layout_grid.attach(self.add_organisation_button,2,row,1,1)
+        self.add_organisation_button.connect("clicked", self.add_organisation_action)
+
+        row += 1
+        
+        self.delete_button=Gtk.Button("Delete", Gtk.STOCK_DELETE)
+        self.delete_button.connect("clicked", self.delete_action)        
+        parent_layout_grid.attach(self.delete_button,0,row,1,1)
+
+        row += 1
+        
+        row=self.overview_component.create_layout(parent_layout_grid, row)
+        
+        row += 1
+        
+        return row
+
+
+    def populate_person_combobox_model(self):
+        combobox_model=Gtk.ListStore(str,str)
+        person_list=Person().get_all()
+        for p in person_list:
+            combobox_model.append(["%s" % p.sid, p.common_name])
+        return combobox_model
+
+    def populate_organisation_combobox_model(self):
+        combobox_model=Gtk.ListStore(str,str)
+        organisation_list=Organisation().get_all()
+        for p in organisation_list:
+            combobox_model.append(["%s" % p.sid, p.common_name])
+        return combobox_model
+
+    
+    def add_person_action(self, widget):
+        # get person sid
+        (current_person_sid,current_person_common_name)=self.get_active_person()
+        # insert originator
+        originator=Originator(common_name=current_person_common_name)
+        originator.insert()
+        # insert forecast_originator
+        forecast_originator = ForecastOriginator(forecast_sid=self.forecast.sid, originator_sid=originator.sid)
+        forecast_originator.insert()
+        # insert originator_person
+        originator_person=OriginatorPerson(originator_sid=originator.sid,person_sid=current_person_sid)
+        originator_person.insert()
+        
+        show_info_dialog("Add successful")
+        self.overview_component.clean_and_populate_model()
+        
+        
+    def delete_action(self, widget):
+        model,tree_iter = self.overview_component.treeview.get_selection().get_selected()
+        (originator_sid)=model.get(tree_iter, 0)
+        Originator(originator_sid).delete()
+        model.remove(tree_iter)   
+        show_info_dialog("Delete successful")   
+        
+        
+    def get_active_person(self):
+        tree_iter = self.person_combobox.get_active_iter()
+        if tree_iter!=None:
+            model = self.person_combobox.get_model()
+            person_sid = model[tree_iter][:2]
+            return person_sid
+        else:
+            print("please choose a person!")
+
+
+
+    def add_organisation_action(self, widget):
+        # get organisation sid
+        (current_organisation_sid,current_organisation_common_name)=self.get_active_organisation()
+        # insert originator
+        originator=Originator(common_name=current_organisation_common_name)
+        originator.insert()
+        # insert forecast_originator
+        forecast_originator = ForecastOriginator(forecast_sid=self.forecast.sid, originator_sid=originator.sid)
+        forecast_originator.insert()
+        # insert originator_person
+        originator_organisation=OriginatorOrganisation(originator_sid=originator.sid,organisation_sid=current_organisation_sid)
+        originator_organisation.insert()
+        
+        show_info_dialog("Add successful")
+        self.overview_component.clean_and_populate_model()
+    
+        
+    def get_active_organisation(self):
+        tree_iter = self.organisation_combobox.get_active_iter()
+        if tree_iter!=None:
+            model = self.organisation_combobox.get_model()
+            organisation_sid = model[tree_iter][:2]
+            return organisation_sid
+        else:
+            print("please choose an organisation!")
+
+
+        
+        
+class OriginatorOverviewComponent(AbstractDataOverviewComponent):
+    
+    treecolumns=[TreeviewColumn("originator_sid", 0, True), TreeviewColumn("person_sid", 1, True), 
+                TreeviewColumn("organisation_sid", 2, True), TreeviewColumn("Typ", 3, False),
+                TreeviewColumn("Common name", 4, False)]
+    
+    def __init__(self, forecast):
+        self.forecast=forecast
+        super(OriginatorOverviewComponent, self).__init__(OriginatorOverviewComponent.treecolumns)
+        
+
+
+    
+    
+    def populate_model(self):
+        self.treemodel.clear()                        
+        cur=get_db_connection().cursor(cursor_factory=psycopg2.extras.NamedTupleCursor)
+        data=(self.forecast.sid,self.forecast.sid,)
+        cur.execute("""SELECT 
+                        fc_person.sid as sid, fc_person.common_name, fc_originator_person.originator_sid,'person' as origin_type 
+                        FROM 
+                        fc_forecast_originator, fc_originator_person, fc_person 
+                        WHERE
+                        fc_forecast_originator.forecast_sid=%s AND 
+                        fc_forecast_originator.originator_sid=fc_originator_person.originator_sid AND
+                        fc_originator_person.person_sid=fc_person.sid
+                        UNION
+                        SELECT 
+                        fc_organization.sid as sid, fc_organization.common_name, fc_originator_organisation.originator_sid,'organisation'  as origin_type  
+                        FROM 
+                        fc_forecast_originator, fc_originator_organisation, fc_organization
+                        WHERE
+                        fc_forecast_originator.forecast_sid=%s AND 
+                        fc_forecast_originator.originator_sid=fc_originator_organisation.originator_sid AND
+                        fc_originator_organisation.organisation_sid=fc_organization.sid
+                        """,data)
+        for p in cur.fetchall():
+            if p.origin_type=='person':
+                self.treemodel.append([ "%s" % p.originator_sid, "%s" % p.sid, None, p.origin_type, p.common_name])
+            elif p.origin_type=='organisation':
+                self.treemodel.append([ "%s" % p.originator_sid, None,"%s" % p.sid, p.origin_type, p.common_name])
+            else:
+                raise Exception("unknown type: %s, expected person or organisation" % p.origin_type)
+        cur.close()
+        
+  
+        
---
+++ predictor/ui/prediction/textmodel/statement/process_component.py
@@ -0,0 +1,139 @@
+"""
+Created on 29.07.2015
+
+@author: vvladych
+"""
+from gi.repository import Gtk
+from predictor.ui.prediction.abstract_data_process_component import AbstractDataManipulationComponent, AbstractDataOverviewComponent
+from predictor.ui.ui_tools import TreeviewColumn, show_info_dialog, DateWidget, TextViewWidget
+import datetime
+from predictor.model.predictor_model import TmstatementDAO
+
+
+class TextmodelStatementManipulationComponent(AbstractDataManipulationComponent):
+    
+    def __init__(self, textmodel, overview_component):
+        super(TextmodelStatementManipulationComponent, self).__init__(overview_component)
+        self.textmodel = textmodel
+        
+    def create_layout(self, parent_layout_grid, row):
+        self.parent_layout_grid = parent_layout_grid
+        row += 1
+        
+        statement_label=Gtk.Label("Statement")
+        parent_layout_grid.attach(statement_label, 0, row, 1, 1)
+        
+        self.prediction_model_textview = Gtk.TextView()
+        self.prediction_model_textview_widget = TextViewWidget(self.prediction_model_textview)
+        
+        parent_layout_grid.attach(self.prediction_model_textview_widget, 1, row, 2, 1)
+
+        row += 2
+                
+        pit_label = Gtk.Label("Choose point-in-time")
+        pit_label.set_justify(Gtk.Justification.LEFT)
+        parent_layout_grid.attach(pit_label,0,row,1,1)
+
+        begin_pit_label = Gtk.Label("Begin")
+        begin_pit_label.set_justify(Gtk.Justification.LEFT)
+        parent_layout_grid.attach(begin_pit_label,1,row,1,1)
+
+        self.state_begin_date_day_textentry = Gtk.Entry()
+        self.state_begin_date_month_textentry = Gtk.Entry()
+        self.state_begin_date_year_textentry = Gtk.Entry()
+
+        self.parent_layout_grid.attach(DateWidget(self.state_begin_date_day_textentry,
+                                                  self.state_begin_date_month_textentry,
+                                                  self.state_begin_date_year_textentry),
+                                       2, row, 1, 1)
+
+        row += 1
+
+        end_pit_label=Gtk.Label("End")
+        end_pit_label.set_justify(Gtk.Justification.LEFT)
+        parent_layout_grid.attach(end_pit_label, 1, row, 1, 1)
+
+        self.state_end_date_day_textentry = Gtk.Entry()
+        self.state_end_date_month_textentry = Gtk.Entry()
+        self.state_end_date_year_textentry = Gtk.Entry()
+
+        self.parent_layout_grid.attach(DateWidget(self.state_end_date_day_textentry,
+                                                  self.state_end_date_month_textentry,
+                                                  self.state_end_date_year_textentry),
+                                       2, row, 1, 1)
+
+        row += 2
+
+        add_statement_button = Gtk.Button("Add", Gtk.STOCK_ADD)
+        parent_layout_grid.attach(add_statement_button, 0, row, 1, 1)
+        add_statement_button.connect("clicked", self.add_statement_action)
+
+        delete_button = Gtk.Button("Delete", Gtk.STOCK_DELETE)
+        delete_button.connect("clicked", self.delete_action)
+        parent_layout_grid.attach(delete_button, 1, row, 1, 1)
+
+        row += 3
+        
+        row = self.overview_component.create_layout(parent_layout_grid, row)
+        
+        row += 1
+
+        return row
+
+    def get_point_in_time_begin(self):
+        return datetime.date(int(self.state_begin_date_year_textentry.get_text()), 
+                             int(self.state_begin_date_month_textentry.get_text()),
+                             int(self.state_begin_date_day_textentry.get_text()))
+
+    def get_point_in_time_end(self):
+        return datetime.date(int(self.state_end_date_year_textentry.get_text()), 
+                             int(self.state_end_date_month_textentry.get_text()),
+                             int(self.state_end_date_day_textentry.get_text()))
+        
+    def get_textmodel_statement_text(self):
+        return self.prediction_model_textview_widget.get_textview_text()
+    
+    def add_statement_action(self, widget):
+        tmstm = TmstatementDAO(None, self.get_textmodel_statement_text(),
+                               self.get_point_in_time_begin(), self.get_point_in_time_end())
+        tmstm.save()
+        self.textmodel.add_tmstatement(tmstm)
+        self.textmodel.save()
+        show_info_dialog(None, "Add successful")
+        self.overview_component.clean_and_populate_model()
+
+    def delete_action(self, widget):
+        model,tree_iter = self.overview_component.treeview.get_selection().get_selected()
+        tmstm = TmstatementDAO(model.get(tree_iter, 0)[0])
+        tmstm.delete()
+        model.remove(tree_iter)
+        self.textmodel.load()
+        show_info_dialog(None, "Delete successful")
+
+
+class TextmodelStatementOverviewComponent(AbstractDataOverviewComponent):
+    
+    treecolumns = [TreeviewColumn("textmodel_statement_uuid", 0, True),
+                   TreeviewColumn("textmodel_uuid", 1, True),
+                   TreeviewColumn("State PIT begin", 2, False),
+                   TreeviewColumn("State PIT end", 3, False),
+                   TreeviewColumn("Statement", 4, False)]
+
+    def __init__(self, textmodel):
+        self.textmodel = textmodel
+        super(TextmodelStatementOverviewComponent, self).__init__(TextmodelStatementOverviewComponent.treecolumns)
+
+    def create_layout(self, parent_layout_grid, row):
+        row += 1
+        
+        self.treeview.set_size_request(200, 150)
+        parent_layout_grid.attach(self.treeview, 0, row, 4, 1)
+                
+        return row
+
+    def populate_model(self):
+        self.treemodel.clear()
+        for tm in self.textmodel.TextmodelToTmstatement:
+            tmstm = TmstatementDAO(tm.secDAO_uuid)
+            tmstm.load()
+            self.treemodel.append(["%s" % tmstm.uuid, "%s" % self.textmodel.uuid, "%s" % tmstm.tmbegin, "%s" % tmstm.tmend, tmstm.text])
