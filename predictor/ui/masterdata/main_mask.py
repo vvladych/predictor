3db2e191c7707e6157a812af7acf1858bdcf0d7c
VVladych <vvladych@yahoo.de>
2016-03-09 23:41:45 +0100

    masterdata refactoring: person exttreeview based implementation, beginning

---
+++ predictor/ui/masterdata/main_mask.py
@@ -0,0 +1,53 @@
+
+from gi.repository import Gtk
+from predictor.ui.masterdata.person.main_mask import PersonMask
+
+class MDMask(Gtk.Grid):
+
+    def __init__(self, main_window):
+        Gtk.Grid.__init__(self)
+
+        self.main_window = main_window
+
+        self.attach(self.mask_chooser(), 0, 0, 1, 1)
+        self.working_area = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
+        self.attach(self.working_area, 0, 1, 1, 1)
+        self.mask_combo.set_active(0)
+
+    def mask_chooser(self):
+        vbox = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
+
+        # add empty Label
+        vbox.pack_start(Gtk.Label(" "), False, False, 0)
+
+        # add mask chooser
+        mask_store = Gtk.ListStore(int, str)
+        mask_store.append([1, "Person"])
+        mask_store.append([2, "Organisation"])
+        mask_store.append([3, "Publisher"])
+        mask_store.append([4, "Object catalog"])
+
+        self.mask_combo = Gtk.ComboBox.new_with_model_and_entry(mask_store)
+        self.mask_combo.set_entry_text_column(1)
+        self.mask_combo.connect("changed", self.mask_combo_changed)
+        vbox.pack_start(self.mask_combo, False, False, 0)
+        return vbox
+
+    def mask_combo_changed(self, mask_combo):
+        print(self.mask_combo.get_active())
+        if self.mask_combo.get_active() == 0:
+            self.set_working_area("person")
+        else:
+            print("unimplemented")
+
+    def clean_working_area(self):
+        for child in self.working_area.get_children():
+            self.working_area.remove(child)
+
+    def set_working_area(self, action="person"):
+        self.clean_working_area()
+        if action == "person":
+            self.working_area.pack_start(PersonMask(self.main_window), True, True, 0)
+        else:
+            print("unimplemented")
+        self.working_area.show_all()
